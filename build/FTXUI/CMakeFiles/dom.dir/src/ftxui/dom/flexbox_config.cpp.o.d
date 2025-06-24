9bf8c4148ff38ba8f3e3ec6c540417eb7bff7120
Azat201003 <zanelane87@gmail.com>
2025-06-25 02:23:50 +0300

    clean

--- build/FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/ubu/Documents/easygram/FTXUI/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/ubu/Documents/easygram/FTXUI/include/ftxui/dom/flexbox_config.hpp
--- build/libs/FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-libs/FTXUI/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/ubu/Documents/easygram/libs/FTXUI/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/ubu/Documents/easygram/libs/FTXUI/include/ftxui/dom/flexbox_config.hpp
