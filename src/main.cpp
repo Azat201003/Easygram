b63dcd3a4202125a23dd86d14454e9a44b2672d3
Azat201003 <zanelane87@gmail.com>
2025-12-21 18:14:59 +0300

    removed telegram.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,15 @@
 
 using namespace ftxui;
 
-ofstream log_file;
-
-void printLog(string text) { log_file << text; }
-
 int main() {
   auto screen = ScreenInteractive::Fullscreen();
 
-  // cout << "hello world";
-  log_file.open("log", std::ios_base::trunc);
-  log_file.close();
-  log_file.open("log", std::ios_base::app);
-  if (!log_file.is_open()) {
-    Logger().system(Logger::SystemMessages::STOP);
-    return 0;
-  }
-
-  Logger *logger = new Logger();
-  // logger->info("dsaf");
-  logger->setPrintFunc(printLog);
-  logger->info("dsaf");
+  Logger *logger = &UniqueLogger::getInstance();
+  logger->setOutputFileAsPrint("log");
   logger->system(Logger::SystemMessages::START);
 
   TgFacade &tg_facade = TgFacade::getInstance(logger);
 
   screen.Loop(getRenderer(screen, logger));
   logger->system(Logger::SystemMessages::STOP);
-  log_file.close();
-  return 0;
 }
